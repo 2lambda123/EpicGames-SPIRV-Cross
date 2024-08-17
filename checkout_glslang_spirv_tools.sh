7c5dc8c17b57d4cbeaeff8e6ab4bdfc848e7c771
Restyled.io <commits@restyled.io>
2024-08-17 13:10:40 +0000

    Restyled by shellcheck

--- checkout_glslang_spirv_tools.sh
+++ checkout_glslang_spirv_tools.sh
@@ -9,41 +9,41 @@ PROTOCOL=https
 
 if [ -d external/glslang ]; then
 	echo "Updating glslang to revision $GLSLANG_REV."
-	cd external/glslang
+	cd external/glslang || exit
 	git fetch origin
 	git checkout $GLSLANG_REV
 else
 	echo "Cloning glslang revision $GLSLANG_REV."
 	mkdir -p external
-	cd external
+	cd external || exit
 	git clone $PROTOCOL://github.com/KhronosGroup/glslang.git
-	cd glslang
+	cd glslang || exit
 	git checkout $GLSLANG_REV
 fi
 cd ../..
 
 if [ -d external/spirv-tools ]; then
 	echo "Updating SPIRV-Tools to revision $SPIRV_TOOLS_REV."
-	cd external/spirv-tools
+	cd external/spirv-tools || exit
 	git fetch origin
 	git checkout $SPIRV_TOOLS_REV
 else
 	echo "Cloning SPIRV-Tools revision $SPIRV_TOOLS_REV."
 	mkdir -p external
-	cd external
+	cd external || exit
 	git clone $PROTOCOL://github.com/KhronosGroup/SPIRV-Tools.git spirv-tools
-	cd spirv-tools
+	cd spirv-tools || exit
 	git checkout $SPIRV_TOOLS_REV
 fi
 
 if [ -d external/spirv-headers ]; then
-	cd external/spirv-headers
+	cd external/spirv-headers || exit
 	git fetch origin
 	git checkout $SPIRV_HEADERS_REV
 	cd ../..
 else
 	git clone $PROTOCOL://github.com/KhronosGroup/SPIRV-Headers.git external/spirv-headers
-	cd external/spirv-headers
+	cd external/spirv-headers || exit
 	git checkout $SPIRV_HEADERS_REV
 	cd ../..
 fi
